958873bafc5cb4afa12df1c1fa1e39d9e547f38c
sigam <sigamatute@gmail.com>
2026-04-16 23:07:11 +0300

    MC Bugs Merge

--- leaf-server/src/main/java/org/dreeam/leaf/config/modules/fixes/MCBugFix.java
+++ leaf-server/src/main/java/org/dreeam/leaf/config/modules/fixes/MCBugFix.java
@@ -9,36 +9,27 @@ public class MCBugFix extends ConfigModules {
         return EnumConfigCategory.FIXES.getBaseKeyName() + ".vanilla-bug-fix";
     }
 
-    public static boolean mc270656 = false;
-    public static boolean mc301114 = false;
+    public static boolean mc270656 = true;
+    public static boolean mc301114 = true;
     public static int mc301114maxCombatEntries = 10240;
 
     @Override
     public void onLoaded() {
         config.addCommentRegionBased(getBasePath(),
-            "Fixes for vanilla Minecraft bugs.",
-            "针对 Minecraft 原版的漏洞修复。"
+            "Fixes for vanilla Minecraft bugs."
         );
-        mc270656 = config.getBoolean(getBasePath() + ".mc-270656", mc270656, config.pickStringRegionBased(
+        mc270656 = config.getBoolean(getBasePath() + ".mc-270656", mc270656,
             """
                 Whether to fix incorrect granting of 'Who needs rockets?' advancement.
-                Mojira link: https://mojira.dev/MC-270656""",
-            """
-                是否修复“还要啥火箭啊？”进度触发的错误检查逻辑。
-                漏洞跟踪器链接：https://mojira.dev/MC-270656"""
-        ));
-        mc301114 = config.getBoolean(getBasePath() + ".mc-301114", mc301114, config.pickStringRegionBased(
+                Mojira link: https://mojira.dev/MC-270656"""
+        );
+        mc301114 = config.getBoolean(getBasePath() + ".mc-301114", mc301114,
             """
                 Whether to fix the memory leak in the combat tracker caused by the mob constantly being damaged.
-                Mojira link: https://mojira.dev/MC-301114""",
-            """
-                是否修复战斗跟踪器（Combat Tracker）中，因生物受到持续性伤害导致的内存泄漏。
-                漏洞跟踪器链接：https://mojira.dev/MC-301114"""
-        ));
-        mc301114maxCombatEntries = config.getInt(getBasePath() + ".mc-301114-max-entries", mc301114maxCombatEntries, config.pickStringRegionBased(
-            "Max allowed entries in mob's combat tracker.",
-            "生物战斗跟踪器中允许的最大条目。"
-        ));
+                Mojira link: https://mojira.dev/MC-301114"""
+        );
+        mc301114maxCombatEntries = config.getInt(getBasePath() + ".mc-301114-max-entries", mc301114maxCombatEntries,
+            "Max allowed entries in mob's combat tracker.");
 
         mc301114maxCombatEntries = Math.max(1, mc301114maxCombatEntries);
     }
